1f635341a390a9060b1ad0a34edeb15729a532d3
Pjuvcke <macsritvars@gmail.com>
2024-11-17 16:09:30 +0200

    Pamatu izveidosana

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Qtn from "./components/Qtn";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h3>Your questionnaires:</h3>
+        <div className="qtn-box">
+          {data.map((qtn) => (
+            <Qtn key={qtn.id} title={qtn.title} content={qtn.content} />
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
